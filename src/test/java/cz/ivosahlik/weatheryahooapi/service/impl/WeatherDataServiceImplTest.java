a012d82cb81110ad765dbe88e171ee1c06c357b8
Ivo Vošahlík <ivosahlik@seznam.cz>
2018-05-13 00:48:22 +0200

    add test

---
+++ src/test/java/cz/ivosahlik/weatheryahooapi/service/impl/WeatherDataServiceImplTest.java
@@ -0,0 +1,16 @@
+package cz.ivosahlik.weatheryahooapi.service.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Intellij Idea
+ * Created by ivosahlik on 13/05/2018
+ */
+public class WeatherDataServiceImplTest {
+
+    @Test
+    public void getCurrentForecastOfCity() {
+    }
+}
